4972205f4477bfcf22acc8ad3e547064daa17703
danascim <davi.nsantos@outlook.com>
2022-09-15 16:53:55 -0300

    First

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,204 @@
+
+
+
+const scriptsInEvents = {
+
+	async ["Moeda-Moeda_Event4_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==runtime.globalVars.Alt3 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==runtime.globalVars.Alt3 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt3==runtime.globalVars.Alt2 || runtime.globalVars.Alt3==runtime.globalVars.Alt1 || runtime.globalVars.Alt3==0)
+		{
+			runtime.globalVars.Alt3 = Math.floor(Math.random() * 3) + 1;
+		}
+	},
+
+	async ["Moeda-Moeda_Event62_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==runtime.globalVars.Alt3 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==runtime.globalVars.Alt3 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt3==runtime.globalVars.Alt2 || runtime.globalVars.Alt3==runtime.globalVars.Alt1 || runtime.globalVars.Alt3==0)
+		{
+			runtime.globalVars.Alt3 = Math.floor(Math.random() * 3) + 1;
+		}
+	},
+
+	async ["Moeda-Moeda_Event80_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 2) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 2) + 1;
+		}
+	},
+
+	async ["Moeda-Moeda_Event117_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 2) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 2) + 1;
+		}
+	},
+
+	async ["Ditado-Extenso_Event56_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==runtime.globalVars.Alt3 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==runtime.globalVars.Alt3 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt3==runtime.globalVars.Alt2 || runtime.globalVars.Alt3==runtime.globalVars.Alt1 || runtime.globalVars.Alt3==0)
+		{
+			runtime.globalVars.Alt3 = Math.floor(Math.random() * 3) + 1;
+		}
+	},
+
+	async ["Ditado-Extenso_Event61_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 2) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 2) + 1;
+		}
+	},
+
+	async ["Soma-Mextenso_Event87_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==runtime.globalVars.Alt3 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 3) + 4;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==runtime.globalVars.Alt3 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 3) + 4;
+		}
+		
+		while(runtime.globalVars.Alt3==runtime.globalVars.Alt2 || runtime.globalVars.Alt3==runtime.globalVars.Alt1 || runtime.globalVars.Alt3==0)
+		{
+			runtime.globalVars.Alt3 = Math.floor(Math.random() * 3) + 4;
+		}
+	},
+
+	async ["Soma-Mextenso_Event92_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 2) + 4;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 2) + 4;
+		}
+	},
+
+	async ["Cditado-Cextenso_Event53_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==runtime.globalVars.Alt3 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==runtime.globalVars.Alt3 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 3) + 1;
+		}
+		
+		while(runtime.globalVars.Alt3==runtime.globalVars.Alt2 || runtime.globalVars.Alt3==runtime.globalVars.Alt1 || runtime.globalVars.Alt3==0)
+		{
+			runtime.globalVars.Alt3 = Math.floor(Math.random() * 3) + 1;
+		}
+	},
+
+	async ["Cditado-Cextenso_Event58_Act1"](runtime, localVars)
+	{
+		runtime.globalVars.Alt1=0;
+		runtime.globalVars.Alt2=0;
+		runtime.globalVars.Alt3=0;
+		
+		while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==0)
+		{
+			runtime.globalVars.Alt1 = Math.floor(Math.random() * 2) + 1;
+		}
+		
+		while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==0)
+		{
+			runtime.globalVars.Alt2 = Math.floor(Math.random() * 2) + 1;
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
